6bf47026f8cbc9db96cc70e11c6388ac228de4df
Roman Iuvshyn <riuvshyn@redhat.com>
2017-06-21 12:52:06 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,6 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'displayName': 'World National Parks (PY)'
-        'displayName': 'National Parks (PY)',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
